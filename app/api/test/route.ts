f08733060a48326b8efaf4f4c9c1d43e4c0fbe99
ezerssss <magbanuaezra@gmail.com>
2024-09-18 10:14:03 +0800

    [Feature][Ezra] added console log

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -6,6 +6,7 @@ export async function GET(request: Request) {
   const testDocs = await testCollection.get();
   const testData: { data: string }[] = [];
   testDocs.forEach((doc) => testData.push(doc.data() as { data: string }));
+  console.log("test");
 
   return Response.json({ testData });
 }
